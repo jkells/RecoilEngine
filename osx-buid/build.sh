dad9ff9d39b38dae478eb6e052e92b45de66b69a
Jared Kells <jkells@gmail.com>
2025-05-23 01:18:05 +0000

    Minimum changes to get CMake to configure on OSX

---
+++ osx-buid/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Run in the parent directory of the script
+# This is needed to be able to run the script from any directory
+cd "$(dirname "$0")/.."
+
+rm -rf build
+mkdir build
+cd build
+
+cmake .. -DMARCH=armv8-a --debug-trycompile
